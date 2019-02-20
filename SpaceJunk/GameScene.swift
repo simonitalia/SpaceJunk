55752a4807792f7168ffb71a06cc5291d73b031e
Simon Italia <sitalia@magicaltomato.com>
2019-02-20 12:54:22 +1100

    - These batch of changes are mostly to add a laser gun to the spaceship to shoot enemy objects which: - Adds a laserBeam button to the scene for player to press - Briefly adds a laser image and laser sound to scene - Explodes and removes enemy object from scene if hit by laser - Added new properties, physicsBody and contactTestBitMask/categoryBitMask to achieve the above - Added new laser beam related assets to app bundle ( button, sound and image) - Renamed existing player assets to spaceShip

--- SpaceJunk/GameScene.swift
+++ SpaceJunk/GameScene.swift
@@ -12,9 +12,14 @@ import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var starfieldBackground: SKEmitterNode!
-    var playerCharacter: SKSpriteNode!
+    
+    //Parent / Container child node properties
+    var spaceShip: SKSpriteNode!
+    var laserButton: SKSpriteNode!
+    var laserBeam = SKSpriteNode()
     
     var enemies = ["ball", "hammer", "tv"]
+    var enemy = SKSpriteNode()
     var gameTimer: Timer!
     var isGameOver = false
     
@@ -25,23 +30,35 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    //Property for handling laserBeam sound
+    var isSpaceShipAlive = true
+    
     override func didMove(to view: SKView) {
+        
+        //Show physics info in scene
+//        view.showsPhysics = true
         
         //Create starfieldBackground object and add to scene
         backgroundColor = UIColor.black
         starfieldBackground = SKEmitterNode(fileNamed: "Starfield")
+        starfieldBackground.name = "starfield"
         starfieldBackground.position = CGPoint(x: 1024, y: 384)
         starfieldBackground.advanceSimulationTime(10)
         starfieldBackground.zPosition = -1
         addChild(starfieldBackground)
         
-        //Create playerCharacter / ship object and add to scene
-        playerCharacter = SKSpriteNode(imageNamed: "player")
-        playerCharacter.position = CGPoint(x:100, y: 384)
-        playerCharacter.physicsBody = SKPhysicsBody(texture: playerCharacter.texture!, size: playerCharacter.size)
-        playerCharacter.physicsBody?.contactTestBitMask = 1
-            //Collision detection configuration
-        addChild(playerCharacter)
+        //Create and add spaceShip to PlayerCharacter container
+        spaceShip = SKSpriteNode(imageNamed: "spaceShip")
+        spaceShip.name = "spaceShip"
+        spaceShip.zPosition = 1
+        
+        //spaceShip Physics
+        spaceShip.physicsBody = SKPhysicsBody(texture: spaceShip.texture!, size: spaceShip.size)
+        spaceShip.position = CGPoint(x:100, y: 384)
+        spaceShip.physicsBody?.isDynamic = false
+        spaceShip.physicsBody?.contactTestBitMask = 1
+            //This value mateches the enemy categoryBitMask
+        addChild(spaceShip)
         
         //Create scoreLabel object and add to scene
         scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
@@ -56,17 +73,72 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         physicsWorld.contactDelegate = self
         
-        //Create gameTimer
+        //Create gameTimer timeInterval: 0.35
         gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
-
+        
+        //Create laserButton object and add to scene
+        laserButton = SKSpriteNode(imageNamed: "laserButton")
+        laserButton.position = CGPoint(x:1024 - 60, y: 768 - 60)
+        laserButton.name = "laserButton"
+        laserButton.zPosition = 0
+        addChild(laserButton)
         
     }
     
+    //Configure firing of laserBeam triggered by touch of laserButton
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-    }
+        //Check if spaceShip is alive
+        if !isSpaceShipAlive {
+            return
+        
+        } else {
+            
+            //Create and add laserBeam to scene
+            guard let touch = touches.first else { return }
+            let touchLocation = touch.location(in: self)
+            let touchedNodes = nodes(at: touchLocation)
+            
+            for touchedNode in touchedNodes {
+                if touchedNode.name == "laserButton" {
+                    
+                    //Create laserBeam (Body A)
+                    laserBeam = SKSpriteNode(imageNamed: "laserBeamGlow")
+                    laserBeam.name = "laserBeam"
+                    laserBeam.zPosition = 0
+                    
+                    //Position laserBeam at front of spaceship relative to playerCharacter container
+                    laserBeam.position = CGPoint(x: 135, y: 0)
+                    
+                    //Give laserBeam a physicsBody
+                    laserBeam.physicsBody = SKPhysicsBody(texture: laserBeam.texture!, size: laserBeam.size)
+                    
+                    //Collision values
+                    laserBeam.physicsBody?.isDynamic = false
+                    laserBeam.physicsBody?.categoryBitMask = 2
+                        //Value matches the enemy contactBitMask
+                    
+                    //Play laserBeam firing sound
+                    run(SKAction.playSoundFileNamed("laserBeamSound", waitForCompletion: false))
+                    
+                    //Add player to playerCharacter container
+                    spaceShip.addChild(laserBeam)
+                    
+                    //Remove laserBeam image and node after x period
+                    let wait = SKAction.wait(forDuration: 0.05)
+                    let removeNode = SKAction.removeFromParent()
+                    laserBeam.run(SKAction.sequence([wait, removeNode]))
+
+                } //End if inner block
+                
+            } //End for loop
+            
+        } //End outer if, else  block
+        
+    } //End touchesBegan()
     
-    //Determine where player touched, and position characterNode at touchedLocation
+    
+    //Determine where player touched and moves characterNode, position characterNode at touchedLocation
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         guard let touched = touches.first else {return}
@@ -76,12 +148,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if touchedLocation.y < 100 {
             touchedLocation.y = 100
         
-        //Clamp all touch points above Y 668 to Y 668
-        } else if touchedLocation.y > 668 {
-            touchedLocation.y = 668
+        //Clamp all touch points above top of play area to top of play area
+        } else if touchedLocation.y > 650 {
+            touchedLocation.y = 650
         }
         
-        playerCharacter.position = touchedLocation
+        //Move playerCharacter container node to touched location
+        spaceShip.position = touchedLocation
         
     }
     
@@ -91,13 +164,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         enemies.shuffle()
         
         //Get enemy from inside array position 0, add to scene
-        let enemy = SKSpriteNode(imageNamed: enemies[0])
-        enemy.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        enemy = SKSpriteNode(imageNamed: enemies[0])
+        enemy.position = CGPoint(x: 1200, y: Int.random(in: 100...580))
+        enemy.name = "enemy"
+        enemy.zPosition = 1
         addChild(enemy)
         
         //Configure enemy physics and contactTestBitMask
         enemy.physicsBody = SKPhysicsBody(texture: enemy.texture!, size: enemy.size)
-        enemy.physicsBody?.contactTestBitMask = 1
+        enemy.physicsBody?.contactTestBitMask = 2
+            //This value matches the laserBeam categoryBitMask
+        enemy.physicsBody?.categoryBitMask = 1
+            //This value matches the spaceShip contactBitMask
+        enemy.physicsBody?.isDynamic = true
         enemy.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
         enemy.physicsBody?.angularVelocity = 5
         enemy.physicsBody?.linearDamping = 0
@@ -121,18 +200,100 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     }//End update()
     
-    //playerCharacter node hit by enemy node handler
-    
+    //playerCharacter node hit by enemy node, and enemy node hit by laserBeam handler
     func didBegin(_ contact: SKPhysicsContact) {
-        let playerCharacterExplode = SKEmitterNode(fileNamed: "explosion")!
-        playerCharacterExplode.position = playerCharacter.position
-        addChild(playerCharacterExplode)
         
-        playerCharacter.removeFromParent()
+        guard let nodeA = contact.bodyA.node else {return}
+        guard let nodeB = contact.bodyB.node else {return}
         
-        //Stop score from updating
-        isGameOver = true
-    }
-    
+        var nodeAName = nodeA.name
+        var nodeBName = nodeB.name
+        
+        //Determine nodeA type
+        switch nodeAName {
+            
+        case "laserBeam":
+            nodeAName = "player"
+            
+        case "spaceShip":
+            nodeAName = "player"
+            
+        case "enemy":
+            nodeAName = "enemy"
+            
+        default:
+            break
+            
+        }
+        
+        //Determine nodeB type
+        switch nodeBName {
+            
+        case "laserBeam":
+            nodeBName = "player"
+            
+        case "spaceShip":
+            nodeBName = "player"
+            
+        case "enemy":
+            nodeBName = "enemy"
+            
+        default:
+            break
+            
+        }
+        
+        //Exit if both nodeAName and nodeBName is player
+        if nodeAName == "player" && nodeBName == "player" {
+            
+            return
+            
+        }
+        
+        //Exit if both nodeAName and nodeBName is enemy
+        if nodeAName == "enemy" && nodeBName == "enemy" {
+            
+            return
+        }
+        
+        //If either nodeNameA or nodeNameB is an enemy node
+        if nodeAName == "enemy" || nodeBName == "enemy"{
+            
+            let explosion = SKEmitterNode(fileNamed: "explosion")!
+            
+            //If enemy hit laserBeam
+            if nodeA.name == "laserBeam" || nodeB.name == "laserBeam" {
+                
+                //Determine which contact.body is the enemy
+                if nodeA.name == "enemy" {
+                    nodeA.removeFromParent()
+                    explosion.position = nodeA.position
+                        //remove enemy from scene
+                
+                } else if nodeB.name == "enemy" {
+                    nodeB.removeFromParent()
+                    explosion.position = nodeB.position
+                        //expode enemy
+                }
+                
+            } else if nodeA.name == "spaceShip" || nodeB.name == "spaceShip" {
+                
+                explosion.position = spaceShip.position
+                spaceShip.removeFromParent()
+                
+                //Silence laserBeam sound since spaceShip exploded
+                isSpaceShipAlive = false
+                
+                //Stop score from updating
+                isGameOver = true
+            }
+            
+        //Add emitter explosion to scene and run explosion sound
+        addChild(explosion)
+        run(SKAction.playSoundFileNamed("explosionSound.caf", waitForCompletion: false))
+            
+        } //End outer if block
+
+    } //End didBegin() method
 }
 
